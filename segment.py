4fa5cf4b647ca3353c0b1538b388f005cb2b61e2
jlee24 <jlee24@stanford.edu>
2017-06-10 21:38:40 -0700

    more segmented style images

--- segment.py
+++ segment.py
@@ -2,14 +2,14 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
-imgname = "train.jpg" # specify image name here
+imgname = "sunset.jpg" # specify image name here
 img = cv2.imread("./input-images/" + imgname) 
 mask = np.zeros(img.shape[:2],np.uint8)
 bgdModel = np.zeros((1,65),np.float64)
 fgdModel = np.zeros((1,65),np.float64)
-rect = (375,470,700,75) # for automated: (1,1,img.shape[1],img.shape[0])
+rect = (150,180,1020,350) # for automated: (1,1,img.shape[1],img.shape[0])
 
-cv2.grabCut(img,mask,rect,bgdModel,fgdModel,5,cv2.GC_INIT_WITH_RECT)
+cv2.grabCut(img,mask,rect,bgdModel,fgdModel,20,cv2.GC_INIT_WITH_RECT)
 mask_fg = np.where((mask==2)|(mask==0),0,1).astype('uint8')
 
 img_masked = img*mask_fg[:,:,np.newaxis] 
