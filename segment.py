f87516f194dd8ef95d969e8d94000bf190cb85fa
jlee24 <jlee24@stanford.edu>
2017-06-10 20:53:44 -0700

    added more segmented input images

--- segment.py
+++ segment.py
@@ -2,12 +2,12 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
-imgname = "mcmurt.jpg" # specify image name here
+imgname = "memchu.jpg" # specify image name here
 img = cv2.imread("./input-images/" + imgname) 
 mask = np.zeros(img.shape[:2],np.uint8)
 bgdModel = np.zeros((1,65),np.float64)
 fgdModel = np.zeros((1,65),np.float64)
-rect = (100,95,830,305) # for automated: (1,1,img.shape[1],img.shape[0])
+rect = (350,50,950,750) # for automated: (1,1,img.shape[1],img.shape[0])
 
 cv2.grabCut(img,mask,rect,bgdModel,fgdModel,5,cv2.GC_INIT_WITH_RECT)
 mask_fg = np.where((mask==2)|(mask==0),0,1).astype('uint8')
